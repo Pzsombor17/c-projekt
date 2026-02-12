bc0c12d001b0423861c427e73442a37bc28fdd05
ParryHotter <Pap.Zsombor@C19-01>
2026-02-12 10:28:50 +0100

    uj

--- c# projekt/ikea.cs
+++ c# projekt/ikea.cs
@@ -13,28 +13,41 @@ namespace asztali_projekt_ikea
         public string color { get; set; }
         public string name { get; set; }
         public int db { get; set; }
-        public bool raktaron { get; set; }
+        //public bool raktaron { get; set; }
         public int price { get; set; }
         public int maxStock { get; set; }
         public Ikea(string[] data)
         {
             this.type = data[0];
-            this.size= double.Parse(data[1]);
-            this.color = data[3];
-            this.name = data[4];
-            this.db = int.Parse(data[5]);
-            this.raktaron = Convert.ToBoolean(data[6]);
-            this.price = int.Parse(data[7]);
-            this.maxStock = int.Parse(data[8]);
+            this.size = double.Parse(data[1]);
+            this.color = data[2];
+            this.name = data[3];
+            this.db = int.Parse(data[4]);
+            //this.raktaron = Convert.ToBoolean(data[6]);
+            this.price = int.Parse(data[5]);
+            this.maxStock = int.Parse(data[6]);
 
         }
         public int buy(int count)
         {
             return price *= count;
         }
+        
+        public int Return(int db,string type)
+        {
+            return 1;
+        }
+        public double discout()
+        {
+            return 1;
+        }
+        public int ReStock()
+        {
+            return 1;
+        }
         public override string ToString()
         {
-            return $"A típús: {type}, méret: {size}, szín: {color}, neve: {name}, elérhető darabszám: {db}, raktáron: {raktaron},ár: {price}, ennyi fér el a raktárban: {maxStock}";
+            return $"A típús: {type}, méret: {size}, szín: {color}, neve: {name}, elérhető darabszám: {db}, raktáron: ,ár: {price}, ennyi fér el a raktárban: {maxStock}";
         }
     }
 }
